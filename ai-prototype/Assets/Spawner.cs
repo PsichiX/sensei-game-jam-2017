40076fc96a40eec365f7621effa6f25ee5fcd0f4
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2017-12-09 00:29:33 +0100

    ai crowd prototype

---
+++ ai-prototype/Assets/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject CrowdPrefab;
+    public GameObject Door;
+    public int CrowdCount = 100;
+    public float Range = 10;
+
+    private void Start()
+    {
+        var pos = transform.position;
+
+        for (int i = 0; i < CrowdCount; ++i)
+        {
+            var offset = Random.insideUnitCircle * Range;
+            var instance = Instantiate(CrowdPrefab);
+            instance.transform.position = pos + new Vector3(offset.x, 0.0f, offset.y);
+            var agent = instance.GetComponent<NavMeshAgent>();
+            agent.SetDestination(Door.transform.position);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
