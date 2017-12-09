36ee071a99af0b07a139dac392d79fb828686055
outfrost <kotlet.bahn@gmail.com>
2017-12-09 16:26:05 +0100

    Add basic target marker for villain routine

--- Shot up/Assets/Scripts/VillainThrowingRoutine.cs
+++ Shot up/Assets/Scripts/VillainThrowingRoutine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -12,12 +13,13 @@ public class VillainThrowingRoutine : Routine
 	public Vector2 TargetAreaStart;
 	public Vector2 TargetAreaEnd;
 	public float MarkerToThrowDelay;
-	public float ThrowToNextMarkerInterval;
+	public float MarkerDisappearDelay = 1.0f;
+	public float NextMarkerDelay;
 	
 	public override void StartRoutine()
 	{
+		Debug.Log("VillainThrowingRoutine.StartRoutine(): throwItem() coroutine starting");
 		StartCoroutine(throwItem());
-		Debug.Log("VillainThrowingRoutine.StartRoutine(): throwItem() coroutine started");
 	}
 
 	private IEnumerator throwItem()
@@ -31,14 +33,16 @@ public class VillainThrowingRoutine : Routine
 					TargetAreaEnd.y));
 			
 			Debug.Log("VillainThrowingRoutine.throwItem(): New target: " + target);
-			
-			// TODO Spawn TargetLocationMarker at target coords
+
+			GameObject markerInstance = Instantiate(TargetLocationMarker, new Vector3(target.x / transform.localScale.x, 0.05f, target.y / transform.localScale.z), Quaternion.identity);
 			
 			yield return new WaitForSecondsRealtime(MarkerToThrowDelay);
 			
 			// TODO Throw ThrowableItem towards target coords
-
-			yield return new WaitForSecondsRealtime(ThrowToNextMarkerInterval); 
+			
+			yield return new WaitForSecondsRealtime(MarkerDisappearDelay);
+			Destroy(markerInstance);
+			yield return new WaitForSecondsRealtime(NextMarkerDelay); 
 		}
 	}
 	
