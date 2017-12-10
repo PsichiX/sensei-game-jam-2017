3eee05975cdb592de31d5b34998d8bf550c5053b
Draggimir <Dmytruszynski@gmail.com>
2017-12-10 01:42:16 +0100

    lights

--- Shot up/Assets/Scripts/SupportScript.cs
+++ Shot up/Assets/Scripts/SupportScript.cs
@@ -4,41 +4,50 @@ using UnityEngine;
 using UnityEngine.AI;
 using System;
 
-public class SupportScript : MonoBehaviour {
-	[Serializable]	
-	public enum Direction {Left, Right, Up, Down};
-	
-	public Direction direction = Direction.Left;
-	public float range = 5;
+public class SupportScript : MonoBehaviour
+{
+    [Serializable]
+    public enum Direction { Left, Right, Up, Down };
 
+    public Direction direction = Direction.Left;
+    public float range = 5;
 
-	void OnTriggerEnter(Collider o)
-	{
-		var other = o;
-		if(other.tag == "Crowd"){
-			var agent = other.gameObject.GetComponent<NavMeshAgent>();
-			if(agent.isActiveAndEnabled)
-			{
-				if(direction == Direction.Left){
-					var target = transform.position + new Vector3(-range, 0, 0);
-					agent.SetDestination(target);
-				}
-				if (direction == Direction.Right)
-				{
-					var target = transform.position + new Vector3(range, 0, 0);
-					agent.SetDestination(target);
-				}
-				if (direction == Direction.Up)
-				{
-					var target = transform.position + new Vector3(0, 0, range);
-					agent.SetDestination(target);
-				}
-				if (direction == Direction.Down)
-				{
-					var target = transform.position + new Vector3(0, 0, -range);
-					agent.SetDestination(target);
-				}
-			}	
-		}
-	}
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.color = Color.white;
+    }
+
+    void OnTriggerEnter(Collider o)
+    {
+        var other = o;
+        if (other.tag == "Crowd")
+        {
+            var agent = other.gameObject.GetComponent<NavMeshAgent>();
+            if (agent.isActiveAndEnabled)
+            {
+                if (direction == Direction.Left)
+                {
+                    var target = transform.position + new Vector3(-range, 0, 0);
+                    agent.SetDestination(target);
+                }
+                if (direction == Direction.Right)
+                {
+                    var target = transform.position + new Vector3(range, 0, 0);
+                    agent.SetDestination(target);
+                }
+                if (direction == Direction.Up)
+                {
+                    var target = transform.position + new Vector3(0, 0, range);
+                    agent.SetDestination(target);
+                }
+                if (direction == Direction.Down)
+                {
+                    var target = transform.position + new Vector3(0, 0, -range);
+                    agent.SetDestination(target);
+                }
+            }
+        }
+    }
 }
